3b9e4621d941e85d3a2c2a279305695fc21930bd
Mel <rodriguezml291@gmail.com>
2019-08-25 16:41:49 -0600

    ejercicio 3 interface

---
+++ ejercicio3/interface.ts
@@ -0,0 +1,16 @@
+interface Shape {
+    name: string;
+    width: number;
+    height: number;
+    color?: string;
+}
+
+function area(shape : Shape) {
+    var area = shape.width * shape.height;
+    return "I'm a " + shape.color + " " + shape.name + " with area " + area + " cm squared";
+}
+
+console.log( area( {name: "rectangle", width: 30, height: 15} ) );
+console.log( area( {name: "square", width: 30, height: 30, color: "blue"} ) );
+document.body.innerHTML = area({name: "rectangle", width: 30, height: 15 });
+document.body.innerHTML = area({name: "square", width: 30, height: 30, color: "blue" });
