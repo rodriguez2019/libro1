e237c847fc8019d54a4d8f89f327d6aec30eb7cf
Mel <rodriguezml291@gmail.com>
2019-08-28 01:08:44 -0600

    ejercicio 7 plantillas

---
+++ ejercicio7/plantillas.js
@@ -0,0 +1,4 @@
+//ejemplo de uso de plantillas de literales
+var e = "building";
+var f = 300;
+var sentence = 'The ${e} in front of my office is ${f} feet tall.';
---
+++ ejercicio7/plantillas.ts
@@ -0,0 +1,5 @@
+//ejemplo de uso de plantillas de literales
+let e: string = "building"; 
+let f: number = 300;
+ 
+let sentence: string = 'The ${e} in front of my office is ${f} feet tall.';
