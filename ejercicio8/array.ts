7bf847f44f7f07fc6794b9c2df68973481509d03
Mel <rodriguezml291@gmail.com>
2019-08-28 01:13:04 -0600

    array

---
+++ ejercicio8/array.ts
@@ -0,0 +1,26 @@
+let a: number[] = [1,12,93,5];
+let b: string[] =["a","apricot","mango"];
+//let c: number[] = [1,"apple","potato"];
+
+let d: Array<number> = [null, undefined, 10, 15]; 
+let ef: Array<string> = ["pie", null, ""];
+
+let ab: number[] = [1, 12, 93, 5]; 
+let bb: string[] = ["a", "apricot", "mango"]; 
+//let cc: number[] = [1, "apple", "potato"]; 
+
+
+let dd: Array<number> = [null, undefined, 10, 15]; 
+let ee: Array<string> = ["pie", null, ""]; 
+
+let one: [number,string] = [11,"monday"];
+//let two:[number,string] =["monday",11];
+//let three:[number,string]=["a","monkey"];
+let four:[number,string]=[105,"owl"];
+let five:[number, string]= [13,"bat"];
+ee[13]="elephant";
+//ee[15]=false;
+
+ console.log(a[1]);
+ console.log(b[2]);
+ console.log(four[1],[2]);
