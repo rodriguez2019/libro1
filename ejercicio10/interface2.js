c11a9e69fe0aed3035fa4727714d0585d051b4ee
Mel <rodriguezml291@gmail.com>
2019-08-28 01:15:29 -0600

    interface2

---
+++ ejercicio10/interface2.js
@@ -0,0 +1,25 @@
+var firstLake = {
+    name: 'Caspian Sea',
+    length: 1199,
+    depth: 1025,
+    area: 371000,
+    isFreshwater: false,
+    countries: ['Kazakhstan', 'Russia', 'Turkmenistan', 'Azerbaijan', 'Iran']
+};
+var secondLake = {
+    name: 'Superior',
+    depth: 406.3,
+    length: 616,
+    area: 82100,
+    isFreshwater: true,
+    countries: ['Canada', 'United States']
+};
+var thirdLake = {
+    name: 'Baikal',
+    depth: 1637,
+    length: 636,
+    area: 31500,
+    isFreshwater: true,
+    countries: ['Russia'],
+    frozen: ['January', 'February', 'March', 'April', 'May']
+};
---
+++ ejercicio10/interface2.ts
@@ -0,0 +1,37 @@
+interface Lakes {
+    name: string,
+    area: number,
+    length: number,
+    depth: number,
+    isFreshwater: boolean,
+    countries: string[],
+    frozen?: string[]
+}
+
+let firstLake: Lakes = {
+    name: 'Caspian Sea',
+    length: 1199,
+    depth: 1025,
+    area: 371000,
+    isFreshwater: false,
+    countries: ['Kazakhstan', 'Russia', 'Turkmenistan', 'Azerbaijan', 'Iran']
+}
+
+let secondLake: Lakes = {
+    name: 'Superior',
+    depth: 406.3,
+    length: 616,
+    area: 82100,
+    isFreshwater: true,
+    countries: ['Canada', 'United States']
+}
+ 
+let thirdLake: Lakes = {
+    name: 'Baikal',
+    depth: 1637,
+    length: 636,
+    area: 31500,
+    isFreshwater: true,
+    countries: ['Russia'],
+    frozen: ['January', 'February', 'March', 'April', 'May']
+}
